fc269416d79a8f6acf19af01c9c441adfa79c8de
John Johnson <dampboxdev@gmail.com>
2013-12-15 21:31:46 -0800

    Finished user actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216002141) do
+ActiveRecord::Schema.define(version: 20131216045800) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20131216002141) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
