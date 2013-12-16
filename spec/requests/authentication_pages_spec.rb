f5c3e9b8c3804baba0d48259910e8415e221a8ba
John Johnson <dampboxdev@gmail.com>
2013-12-15 17:19:55 -0800

    Finished sign in

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+	
+	 
+    end
+  end
+end
